d17a8290bdc06d4eeb5760ad3272256644d7e9bc
Sousa Gaspar <sousadgaspar@gmail.com>
2020-05-28 22:23:40 +0100

    first commit

---
+++ src/components/MyParagraph.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MyParagraph(){
+    return (
+        <p>
+          Hey React App. This is a completely different staff
+        </p>
+    );
+}
+
+export default MyParagraph

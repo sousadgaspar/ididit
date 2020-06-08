5144253112ac078c0e71743a35e65cfa8171e96a
Sousa Gaspar <sousadgaspar@gmail.com>
2020-06-08 20:56:14 +0100

    add events on todoItem component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 import TodoItem from "./Todo/TodoItem";
 
+class Tasks extends Component {
 
-let tasks = {
-    padding: 0,
-    margin: 0,
-    listStyleType: "none"
-}
+    constructor () {
+        super();
 
-let payload = {
-    task: "Clean the room",
-    status: ""
-}
-
-class Tasks {
+        this.state = {
+            task: "Clean the room",
+            status: ""
+        }
+    }
 
     render () {
+
+        let tasks = {
+            padding: 0,
+            margin: 0,
+            listStyleType: "none"
+        }
+        
+        
         return (<div className="todo-list">
-                    <TodoItem payload={payload} />
-                    <TodoItem payload={payload} />
-                    <TodoItem payload={payload} />
-                    <TodoItem payload={payload} />
+                    <TodoItem payload={this.state.task} />
+                    <TodoItem payload={this.state.task} />
+                    <TodoItem payload={this.state.task} />
+                    <TodoItem payload={this.state.task} />
                 </div>);
     }
 }
--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -6,7 +6,7 @@ class TodoItem extends React.Component {
     render () {
         return (
             <div className="todo-item">
-                <input type="checkbox" checked={props.payload.status} />
+                <input type="checkbox" checked={this.props.payload.status} />
                 <p>{this.props.payload.task}</p>
             </div>
         );
